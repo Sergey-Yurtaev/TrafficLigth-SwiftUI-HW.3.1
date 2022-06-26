5f57e7ad36c7f2e880f56fde8f15a19282748688
Sergey-Yurtaev <engineerk2@mail.ru>
2022-06-26 11:10:01 +0500

    Initial Commit

---
+++ TrafficLigth(SwiftUI)HW.3.1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrafficLigth(SwiftUI)HW.3.1
+//
+//  Created by Sergey Yurtaev on 26.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrafficLigth(SwiftUI)HW.3.1/TrafficLigth_SwiftUI_HW_3_1App.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLigth_SwiftUI_HW_3_1App.swift
+//  TrafficLigth(SwiftUI)HW.3.1
+//
+//  Created by Sergey Yurtaev on 26.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLigth_SwiftUI_HW_3_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
